760cd2469453e90b9c20827f723e3154b6ca5b5d
VitaliyM <vetalmamchur@gmail.com>
2023-04-13 13:49:11 +0000

    Header

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './navbar.css';
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className='navLogo'></div>
-        <div className='navListItems'></div>
-        <div className='navConsultation'></div>
+    <nav className='navBlock'>
+        <div className='navLogo'>
+            <span className='navLogoBigText'>DubaiRealty</span>
+            <span className='navLogoSmallText'>Real Estate</span>
+        </div>
+
+        <div className='navListItems'>
+            <a href="##" target="_blank" className='navList'>Buy</a>
+            <a href="##" target="_blank" className='navList'>Blog</a>
+            <a href="##" target="_blank" className='navList'>About</a>
+            <a href="##" target="_blank" className='navList'>Contact</a>
+        </div>
+
+        <div className='navConsultation'>
+            <button className='navConsultButton'>Book a consultation</button>
+            <a href="tel:+971001112233" className='navConsultNumber'>+971 00 111 2233</a>
+        </div>
     </nav>
   )
 }
