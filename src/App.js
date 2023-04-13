77d305c557638b196f073e0a0d7cb3c397703eaf
VitaliyM <vetalmamchur@gmail.com>
2023-04-13 13:12:42 +0000

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       
+      <Header />
+      {/* <Projects />
+      <Apartment />
+      <ContactUs />
+      <Articles />
+      <Owner />
+      <Faq />
+      <Footer /> */}
+
     </div>
   );
 }
---
+++ src/components/header/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='navLogo'></div>
+        <div className='navListItems'></div>
+        <div className='navConsultation'></div>
+    </nav>
+  )
+}
+
+export default Navbar
