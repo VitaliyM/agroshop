18853ced52c4b619ccb696e4c132c6eac69e96b4
VitaliyM <vetalmamchur@gmail.com>
2023-04-17 19:47:52 +0000

    Styling Header

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -4,18 +4,17 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <nav className='navBlock'>
-        <div className='navListItems'>
-            <a href="##" className='navList navListBuy'>Buy
-              <div className='navListBuyCategory'>
-                <a href="##" className='categoryList'>Category number one</a>
-                <a href="##" className='categoryList'>Apartments in Dubai</a>
-                <a href="##" className='categoryList'>Category number twenty five</a>
-              </div>
-            </a>
-            <a href="##" className='navList'>Blog</a>
-            <a href="##" className='navList'>About</a>
-            <a href="##" className='navList'>Contact</a>
+      <a href="##" className='navList navListBuy'>Buy
+        <div className='menuArrow'></div>
+        <div className='navListBuyCategory'>
+          <a href="##" className='categoryList'>Category number one</a>
+          <a href="##" className='categoryList'>Apartments in Dubai</a>
+          <a href="##" className='categoryList'>Category number twenty five</a>
         </div>
+      </a>
+      <a href="##" className='navList'>Blog</a>
+      <a href="##" className='navList'>About</a>
+      <a href="##" className='navList'>Contact</a>
     </nav>
   )
 }
