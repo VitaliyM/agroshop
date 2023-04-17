4dbc0e43407bec196a74e6baff116e057dc8f46a
VitaliyM <vetalmamchur@gmail.com>
2023-04-17 13:43:56 +0000

    1

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -4,11 +4,6 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <nav className='navBlock'>
-        {/* <div className='navLogo'>
-            <span className='navLogoBigText'>DubaiRealty</span>
-            <span className='navLogoSmallText'>Real Estate</span>
-        </div> */}
-
         <div className='navListItems'>
             <a href="##" className='navList navListBuy'>Buy
               <div className='navListBuyCategory'>
@@ -21,11 +16,6 @@ const Navbar = () => {
             <a href="##" className='navList'>About</a>
             <a href="##" className='navList'>Contact</a>
         </div>
-
-        {/* <div className='navConsultation'>
-            <button className='navConsultButton'>Book a consultation</button>
-            <a href="tel:+971001112233" className='navConsultNumber'>+971 00 111 2233</a>
-        </div> */}
     </nav>
   )
 }
