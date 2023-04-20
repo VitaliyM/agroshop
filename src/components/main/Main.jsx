557cfb29cfcd56c5fb109a11698b9040acc505f1
VitaliyM <vetalmamchur@gmail.com>
2023-04-20 13:47:24 +0000

    Slider block

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.css';
+import Presentation from './presentation/Presentation'
+
+const Main = () => {
+  return (
+    <div className='mainBlock'>
+        <Presentation />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/main/presentation/Presentation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './presentation.css';
+
+const Presentation = () => {
+  return (
+    <div className='presentBlock'>Presentation
+        <div>Text</div>
+        <div>Socials</div>
+        <div>Switch</div>
+    </div>
+  )
+}
+
+export default Presentation
