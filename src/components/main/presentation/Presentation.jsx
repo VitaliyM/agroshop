35bdf142bc7e6ef47efe8af6355e9ce1e73190f6
VitaliyM <vetalmamchur@gmail.com>
2023-04-21 13:52:59 +0000

    Present block Styling

--- src/components/main/presentation/Presentation.jsx
+++ src/components/main/presentation/Presentation.jsx
@@ -3,9 +3,19 @@ import './presentation.css';
 
 const Presentation = () => {
   return (
-    <div className='presentBlock'>Presentation
-        <div>Text</div>
-        <div>Socials</div>
+    <div className='presentBlock'>
+        <div className='presentText'>
+          <div className='presentTextHeader'>Lorem ipsum</div>
+          <div className='presentTextDescribe'>Welcome Home <span>To</span> Luxury</div>
+          <div className='presentTextConsult'>Book a consultation</div>
+        </div>
+        <div className='presentSocials'>
+          <div className="presentSocialsBtn facebook"></div>
+          <div className="presentSocialsBtn twitter"></div>
+          <div className="presentSocialsBtn youtube"></div>
+          <div className="presentSocialsBtn instagram"></div>
+          <div className="presentSocialsBtn linkedIn"></div>
+        </div>
         <div>Switch</div>
     </div>
   )
