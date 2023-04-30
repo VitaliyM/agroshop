d9cbd71a09fa476a2ee90453bb48d362d0997877
VitaliyM <vetalmamchur@gmail.com>
2023-04-30 09:50:47 +0000

    Slider styling 2

--- src/components/main/latestProjects/LatestProjects.jsx
+++ src/components/main/latestProjects/LatestProjects.jsx
@@ -4,18 +4,31 @@ import './latestProjects.css'
 const LatestProjects = () => {
   return (
     <div className='latestProjectsBlock'>
-        <div className='projectsNav'>
-            <h2 className='projectsHeader'>Latest projects</h2>
-            <div className='projectsMenuBlock'>
-                <div className='projectsMenuList'>All</div>
-                <div className='projectsMenuList'>Building</div>
-                <div className='projectsMenuList'>Interior</div>
-                <div className='projectsMenuList'>View all projects</div>
-            </div>
+      <div className='projectsNav'>
+        <h2 className='projectsHeader'>Latest projects</h2>
+        <div className='projectsMenuBlock'>
+          <div className='projectsMenuList'>All</div>
+          <div className='projectsMenuList'>Building</div>
+          <div className='projectsMenuList'>Interior</div>
+          <div className='projectsMenuList'>View all projects</div>
         </div>
+      </div>
 
-        <div className='projectsSliderBlock'></div>
-        <div className='projectsExpertiseBlock'></div>
+      <div className='projectsSliderBlock'>
+        <div className='rpojectListWrapper'>
+          <div className="projectList project1"></div>
+          <div className="projectList project2"></div>
+          <div className="projectList project3"></div>
+          <div className="projectList project4"></div>
+        </div>
+        <div className='switchSliderBlock'>
+          <div className="switchSlide"></div>
+          <div className="switchSlide"></div>
+          <div className="switchSlide"></div>
+        </div>
+      </div>
+
+      <div className='projectsExpertiseBlock'></div>
     </div>
   )
 }
