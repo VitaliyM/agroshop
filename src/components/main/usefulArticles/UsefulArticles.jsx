4e0cc8155c1dc48ec3416236f14600bf1ef34bd5
VitaliyM <vetalmamchur@gmail.com>
2023-05-19 14:04:35 +0000

    Useful Styling

--- src/components/main/usefulArticles/UsefulArticles.jsx
+++ src/components/main/usefulArticles/UsefulArticles.jsx
@@ -5,16 +5,34 @@ const UsefulArticles = () => {
     return (
         <div className='usefulArtBlock'>
             <div className='usefulArtHeader'>
-                <div className='usefulArtBigText'>useful articles</div>
+                <div className='usefulArtBigText'>Useful articles</div>
                 <div className='usefulArtViewAll'>View all articles</div>
             </div>
+
             <div className='usefulArtWrapper'>
                 <div className='usefulArticle article1'>
-                    <div className='articleHeader'>Discover Architecture</div>
                     <div className='usefulArticleInfo'>
                         <div className='articleInfoDate'>Jule 03, 2022</div>
                         <div className='articleInfoHeader'>Discover Architecture</div>
-                        <div className='articleInfoText'>Projects for many large domestic and 
+                        <div className='articleInfoText'>Projects for many large domestic and
+                            foreign corporations, enterprises in many elds such</div>
+                        <div className='articleInfoLearnMore'>Learn more</div>
+                    </div>
+                </div>
+                <div className='usefulArticle article2'>
+                    <div className='usefulArticleInfo'>
+                        <div className='articleInfoDate'>Jule 03, 2022</div>
+                        <div className='articleInfoHeader'>Discover Architecture</div>
+                        <div className='articleInfoText'>Projects for many large domestic and
+                            foreign corporations, enterprises in many elds such</div>
+                        <div className='articleInfoLearnMore'>Learn more</div>
+                    </div>
+                </div>
+                <div className='usefulArticle article3'>
+                    <div className='usefulArticleInfo'>
+                        <div className='articleInfoDate'>Jule 03, 2022</div>
+                        <div className='articleInfoHeader'>Discover Architecture</div>
+                        <div className='articleInfoText'>Projects for many large domestic and
                             foreign corporations, enterprises in many elds such</div>
                         <div className='articleInfoLearnMore'>Learn more</div>
                     </div>
