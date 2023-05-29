fab3c25ea086a3232040f7aeba6a77229a1e1eea
VitaliyM <vetalmamchur@gmail.com>
2023-05-29 12:48:16 +0000

    Investor Block Add

---
+++ src/components/main/investorsSlider/InvestorsSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './investorsSlider.css';
+
+const InvestorsSlider = () => {
+  return (
+    <div className='investorsBlock'>
+
+    </div>
+  )
+}
+
+export default InvestorsSlider
