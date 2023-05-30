d71976b11cc5f41a60d163dde35db5b56981d877
VitaliyM <vetalmamchur@gmail.com>
2023-05-30 14:12:54 +0000

    Investor Block Styling

--- src/components/main/investorsSlider/InvestorsSlider.jsx
+++ src/components/main/investorsSlider/InvestorsSlider.jsx
@@ -4,44 +4,48 @@ import Image1 from '../../../images/investor1.png';
 import Image2 from '../../../images/investor2.png';
 
 const InvestorsSlider = () => {
-  return (
-    <div className='investorsBlock'>
-        <div className="investorsSliderWrap">
-            <div className="investorSlide">
-                <img src={Image2} alt="investor1" className='investor investor1'/>
-                <div className="quotes"></div>
-                <div className="investorName"></div>
-                <div className="investorCompany"></div>
-                <div className="investorText"></div>
+    return (
+        <div className='investorsBlock'>
+            <div className="investorsSliderWrap">
+                <div className="investorSlide">
+                    <img src={Image2} alt="investor1" className='investor investor1' />
+                    <div className="quotes"></div>
+                    <div className="investorName"></div>
+                    <div className="investorCompany"></div>
+                    <div className="investorText"></div>
+                </div>
+
+                <div className="investorSlide">
+                    <img src={Image1} alt="investor1" className='investor investor1' />
+                    <div className="quotes"></div>
+                    <div className="investorName"></div>
+                    <div className="investorCompany"></div>
+                    <div className="investorText"></div>
+                </div>
+
+                <div className="investorSlide">
+                    <img src={Image2} alt="investor1" className='investor investor1' />
+                    <div className="quotes"></div>
+                    <div className="investorName"></div>
+                    <div className="investorCompany"></div>
+                    <div className="investorText"></div>
+                </div>
+
+                <div className="investorSlide">
+                    <img src={Image1} alt="investor1" className='investor investor1' />
+                    <div className="quotes"></div>
+                    <div className="investorName"></div>
+                    <div className="investorCompany"></div>
+                    <div className="investorText"></div>
+                </div>
             </div>
-           
-            <div className="investorSlide">
-                <img src={Image1} alt="investor1" className='investor investor1'/>
-                <div className="quotes"></div>
-                <div className="investorName"></div>
-                <div className="investorCompany"></div>
-                <div className="investorText"></div>
+            <div className='investorSlideSwitcher'>
+                <div className="investorSwitch investorSwitchBtn1"></div>
+                <div className="investorSwitch investorSwitchBtn1"></div>
+                <div className="investorSwitch investorSwitchBtn1"></div>
             </div>
-           
-            <div className="investorSlide">
-                <img src={Image2} alt="investor1" className='investor investor1'/>
-                <div className="quotes"></div>
-                <div className="investorName"></div>
-                <div className="investorCompany"></div>
-                <div className="investorText"></div>
-            </div>
-           
-            <div className="investorSlide">
-                <img src={Image1} alt="investor1" className='investor investor1'/>
-                <div className="quotes"></div>
-                <div className="investorName"></div>
-                <div className="investorCompany"></div>
-                <div className="investorText"></div>
-            </div>
-           
         </div>
-    </div>
-  )
+    )
 }
 
 export default InvestorsSlider
