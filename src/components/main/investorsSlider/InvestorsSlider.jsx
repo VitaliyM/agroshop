5cb9d5a64c45d8ad10b69d97654d6f01450f4447
VitaliyM <vetalmamchur@gmail.com>
2023-06-02 13:33:34 +0000

    Investots Block Styling

--- src/components/main/investorsSlider/InvestorsSlider.jsx
+++ src/components/main/investorsSlider/InvestorsSlider.jsx
@@ -10,33 +10,37 @@ const InvestorsSlider = () => {
                 <div className="investorSlide">
                     <img src={Image2} alt="investor1" className='investor investor1' />
                     <div className="quotes"></div>
-                    <div className="investorName"></div>
-                    <div className="investorCompany"></div>
-                    <div className="investorText"></div>
+                    <div className="investorName">Robert</div>
+                    <div className="investorCompany">Founder in <a href='##' className='investorLink'> Apple inc.</a></div>
+                    <div className="investorText">Founded in 2007, Sparch is specializing in providing innovative services such as website design, brand identity and marketing
+                    </div>
                 </div>
 
                 <div className="investorSlide">
                     <img src={Image1} alt="investor1" className='investor investor1' />
                     <div className="quotes"></div>
-                    <div className="investorName"></div>
-                    <div className="investorCompany"></div>
-                    <div className="investorText"></div>
+                    <div className="investorName">Paul</div>
+                    <div className="investorCompany">Owner in <a href='##'                      className='investorLink'>Paul.com</a></div>
+                    <div className="investorText">Projects for many large domestic and foreign corporations, enterprises in many elds such as nance, banking, F&B, education, communication.
+                    </div>
                 </div>
 
                 <div className="investorSlide">
                     <img src={Image2} alt="investor1" className='investor investor1' />
                     <div className="quotes"></div>
-                    <div className="investorName"></div>
-                    <div className="investorCompany"></div>
-                    <div className="investorText"></div>
+                    <div className="investorName">Robert</div>
+                    <div className="investorCompany">Founder in <a href='##' className='investorLink'>Apple inc.</a></div>
+                    <div className="investorText">Founded in 2007, Sparch is specializing in providing innovative services such as website design, brand identity and marketing
+                    </div>
                 </div>
 
                 <div className="investorSlide">
                     <img src={Image1} alt="investor1" className='investor investor1' />
                     <div className="quotes"></div>
-                    <div className="investorName"></div>
-                    <div className="investorCompany"></div>
-                    <div className="investorText"></div>
+                    <div className="investorName">Paul</div>
+                    <div className="investorCompany">Owner in <a href='##' className='investorLink'>Paul.com</a></div>
+                    <div className="investorText">Projects for many large domestic and foreign corporations, enterprises in many elds such as nance, banking, F&B, education, communication.
+                    </div>
                 </div>
             </div>
             <div className='investorSlideSwitcher'>
