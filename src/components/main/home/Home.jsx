16a1bf87ba541c4742de29376aa841022e299953
VitaliyM <vetalmamchur@gmail.com>
2024-02-26 15:13:04 +0000

    Change Contact Us

--- src/components/main/home/Home.jsx
+++ src/components/main/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LatestProjects from './latestProjects/LatestProjects';
 import VideoApartments from './videoApartments/VideoApartments';
 import DontLimit from './dontLimit/DontLimit';
-import ContactUs from './contactUs/ContactUs';
+import ContactUs from './../contactUs/ContactUs';
 import UsefulArticles from './usefulArticles/UsefulArticles';
 import InvestorsSlider from './investorsSlider/InvestorsSlider';
 import Presentation from './presentation/Presentation';
